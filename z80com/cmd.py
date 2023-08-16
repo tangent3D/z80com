f7b3ac0170f93c49393b410487085db4072cdca5
tangent3D <interfectum@gmail.com>
2023-08-16 02:32:40 -0700

    Dump feature, CTS timeout, cleanup

--- z80com/cmd.py
+++ z80com/cmd.py
@@ -8,33 +8,55 @@ def main(argv=None):
     parser = argparse.ArgumentParser(
         description="ph"
     )
-    parser.add_argument("--file", "-f", required=True, metavar="FILE", help="ph")
     parser.add_argument("--port", "-p", required=True, help="ph")
+    parser.add_argument("--file", "-f", required=False, metavar="FILE", help="ph")
+    parser.add_argument("--dump", "-d", required=False, help="ph", action='store_true')
+    parser.add_argument("--output", "-o", required=False, help="ph", default="dump.bin")
 
+    global args
     args = parser.parse_args(argv)
 
-    try:
-        ser = serial.Serial(args.port, 115200, rtscts=True, timeout=5, write_timeout=5)
-    except:
-        print("Could not open " + args.port + ".")
-        end()
-
-    with open(args.file, 'rb') as fh:
-        try:
-            ser.write(fh.read())
-        except:
-            print("Transfer failed.")
-            end()
-        print("Transfer complete.")
-        end()
-
-def end():
-    time.sleep(0.25)
-    try:
+    # Open port and transfer contents of file
+    if args.file != None:
+        openPort()
+        with open(args.file, 'rb') as fh:
+            try:
+                ser.write(fh.read())
+                # Allow serial device time to finish writing!
+                time.sleep(1)
+                print("Transfer complete.")
+            except:
+                print("Transfer failed.")
         ser.close()
-    except:
-        pass
-    exit()
+
+    # Open port and dump received data to file
+    if args.dump == True:
+        openPort()
+        buffer = []
+        buffer = ser.read(65536)
+        with open(args.output, 'wb') as fh:
+            fh.write(buffer)
+            print("Wrote " + str(len(buffer)) + " bytes to " + args.output + ".")
+        ser.close()
+
+# Open serial port specified by argument
+def openPort():
+    global ser
+    try:
+        ser = serial.Serial(args.port, 115200, rtscts=True, timeout=1, write_timeout=1)
+
+        # Wait for ZF to assert RTS, exit if timeout
+        t = 3
+        while (ser.cts != True):
+            time.sleep(1)
+            t -= 1
+            if (t <= 0):
+                print("Request timed out. Device did not assert RTS.")
+                exit()
+
+    except serial.SerialException:
+        print("Could not open " + args.port + ".")
+        exit()
 
 if __name__ == "__main__":
     main()
